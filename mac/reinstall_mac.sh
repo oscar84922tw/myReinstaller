4352d7207f524f1a254bf6ae24d25cb857a94e0f
chenhuawei <chenhuawei@chenhuaweis-MacBook-Pro.local>
2018-08-16 20:58:09 +0800

    update

--- mac/reinstall_mac.sh
+++ mac/reinstall_mac.sh
@@ -23,16 +23,16 @@ brew update
 brew upgrade --all
 
 # Install packages
-apps=( autoconf automake cmake ctags ffmpeg gcc git go gradle highlight htop jpeg libdnet libevent libffi libmpc libpng libtiff libtool libyaml nmap node numpy mycli macvim  openssl pcre pixman pkg-config polipo portaudio python python3 r rkhunter ruby smartmontools source-highlight sqlite ssh-copy-id testdisk tor tig tmux valgrind vim wget zsh zsh-syntax-highlighting z )
+apps=( wget autoconf automake cmake ctags ffmpeg gcc git go gradle highlight htop nmap node mycli macvim  openssl python python3  rkhunter ruby smartmontools source-highlight sqlite ssh-copy-id tor tig tmux valgrind vim wget zsh zsh-syntax-highlighting z )
 
-# caskapps=( appcleaner arduino atom dropbox firefox google-chrome iterm2 java mplayerx mysqlworkbench processing robomongo quicklook-csv quicklook-json skype spotify sqlitebrowser vlc webpquicklook rstudio)
+caskapps=( appcleaner dropbox firefox google-chrome iterm2 java mplayerx mysqlworkbench quicklook-csv quicklook-json skype spotify sqlitebrowser vlc webpquicklook docker )
 
 pythonFramework=( glances )
 pip3 install "${pythonFramework[@]}"
 brew install "${apps[@]}"
 brew tap caskroom/versions
 brew update
-# brew cask install --appdir="/Applications" "${caskapps[@]}"
+brew cask install --appdir="/Applications" "${caskapps[@]}"
 brew cleanup
 brew cask cleanup
 sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/sbin/airport
