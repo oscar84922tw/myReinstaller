99b2c05d4fe1a57d7d3ab153730f867a8a7839f7
Cornelious Chen (RD-TW) <cornelious_chen@tw-corneliousch.client.tw.trendnet.org>
2018-08-27 11:48:49 +0800

    change tmux template and change reinstall script for mac

--- mac/reinstall_mac.sh
+++ mac/reinstall_mac.sh
@@ -21,43 +21,47 @@ fi
 brew tap homebrew/versions
 brew update
 brew upgrade --all
-
+brew bundle
 # Install packages
-apps=( wget autoconf automake cmake ctags ffmpeg gcc git go gradle highlight htop nmap node mycli macvim openssl pcre pixman pkg-config polipo portaudio python python3 rkhunter ruby smartmontools source-highlight sqlite ssh-copy-id tig tmux valgrind vim zsh zsh-syntax-highlighting z ack )
+# apps=( wget autoconf automake cmake ctags ffmpeg gcc git go gradle highlight htop nmap node mycli macvim openssl pcre pixman pkg-config polipo portaudio python python3 rkhunter ruby smartmontools source-highlight sqlite ssh-copy-id tig tmux valgrind vim zsh zsh-syntax-highlighting z ack )
 
-caskapps=( appcleaner firefox google-chrome iterm2 mysqlworkbench quicklook-csv quicklook-json spotify sqlitebrowser webpquicklook docker )
+caskapps=( appcleaner firefox google-chrome iterm2 mysqlworkbench quicklook-csv quicklook-json spotify webpquicklook docker )
 
-pythonFramework=( glances )
-pip3 install "${pythonFramework[@]}"
-brew install "${apps[@]}"
-brew tap caskroom/versions
+pythonFramework=( glances virtualenvwrapper )
+# brew install "${apps[@]}"
+# brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
 brew update
 brew cask install --appdir="/Applications" "${caskapps[@]}"
 brew cleanup
 brew cask cleanup
+pip install "${pythonFramework[@]}"
 
+# virtualenvwrapper setup 
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+cp zshrc ~/.zshrc
 
-cp ./zshrc ~/.zshrc
 # install vim
 wget clone https://raw.githubusercontent.com/ipomoealba/Pyvim/master/.vimrc ~/.vimrc
 
 # install oh my tmux 
-cd ~ && git clone https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf
-wget https://raw.githubusercontent.com/oscar84922tw/myReinstaller/master/mac/tmux.conf.local ~/.tmux.conf.local
-# 電池顯示是百分百
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
 defaults write com.apple.menuextra.battery -bool true
 
-# Finder 顯示狀態欄
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder 顯示地址欄
 defaults write com.apple.finder ShowPathbar -bool true
 
-# 禁止在網絡驅動器上生成 .DS_Store 文件
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
